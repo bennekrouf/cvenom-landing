f670ce496810977a850b05f1332fb2246b4aea16
Bennekrouf <mohamed.bennekrouf@gmail.com>
2025-10-22 10:54:53 +0200

    Try other approach for plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import Script from 'next/script'
 import './globals.css'
 import { ReactNode } from 'react'
+import { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Cvenom - AI-Powered CV Sidekick',
   description: 'Transform CV chaos into career confidence. Multilingual, AI-driven, ATS-optimized.',
 }
@@ -20,18 +21,6 @@ export default function RootLayout({
           data-domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}
           src="https://plausible.io/js/script.outbound-links.js"
         />
-        <Script
-          src="https://plausible.io/js/pa-I2L6va00JukQOR_ZLk8dy.js"
-          strategy="afterInteractive"
-        />
-        <Script id="plausible-setup" strategy="afterInteractive">
-          {`
-    window.plausible = window.plausible || function() { 
-      (window.plausible.q = window.plausible.q || []).push(arguments);
-    };
-    plausible.init();
-  `}
-        </Script>
       </head>
       <body>{children}</body>
     </html>
